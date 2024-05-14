da1afb3681aeed5681f13c82ff551cf5a3d426a6
edgardojr09 <128987307+edgardojr09@users.noreply.github.com>
2024-05-14 13:56:04 +0800

    lesson2.sol

--- lesson2.sol
+++ lesson2.sol
@@ -1,26 +1,25 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Mathematics {
+contract Calcu {
 
-    function addition(uint256 a, uint256 b) public pure returns (uint256) {
-        return a + b;
+    function add(uint256 e, uint256 o) public pure returns (uint256) {
+        return e + o;
     }
     
 
-    function subtraction(uint256 a, uint256 b) public pure returns (uint256) {
-        return a - b;
+    function subtract(uint256 e, uint256 o) public pure returns (uint256) {
+        return  e - o;
     }
     
 
-    function multiplication(uint256 a, uint256 b) public pure returns (uint256) {
-        return a * b;
+    function multiply(uint256 e, uint256 o) public pure returns (uint256) {
+        return e * o;
     }
     
 
-    function division(uint256 a, uint256 b) public pure returns (uint256) {
-        require(b != 0, "Division by zero");
-        return a / b;
+    function divided(uint256 e, uint256 o) public pure returns (uint256) {
+        require(o != 0, "Divided by zero");
+        return e / o;
     }
 }
